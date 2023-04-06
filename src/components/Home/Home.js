f451c032d26f85640506a35fb9a6815fb9c585b5
nimra <nimra@myyogateacher.com>
2023-04-06 21:56:55 +0500

    create redux-toolkit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,24 @@
 import React, {useEffect} from 'react'
 import MovieListing from "../MovieListing/MovieListing";
+// import movieApi from "../../common/apis/movieApi";
+// import {APIKey} from "../../common/apis/MovieApiKey";
 
 const Home = () => {
 
   useEffect(() => {
-    const fetchMovie = async = () => {
-      const response = await movieApi.get(`?`)
-    };
-  });
-  
+    // const movieText = "Harry";
+    // const fetchMovie = async () => {
+    //   const response = await movieApi
+    //   .get(`?apiKey=${APIKey}&s=${movieText}&type`)
+    //   .catch((err)=> {
+    //     console.log("Err :", err)
+    //   });
+    //   console.log("The response from API", response);
+    // };
 
+    // fetchMovie();
+  },[]);
+  
   return (
     <>
       <div className="banner-img"></div>
